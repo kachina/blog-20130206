6c1175990db1c5431e570df3a42b1edbac761833
kachina_t <kachina.t@gmail.com>
2013-02-05 19:31:02 +0900

    Regionの指定に誤りがあったので修正しました

--- terminate.php
+++ terminate.php
@@ -2,5 +2,5 @@
   require_once('AWSSDKforPHP/sdk.class.php');
 
   $ec2 = new AmazonEC2(array('default_cache_config' => '/tmp'));
-  $ec2->set_region(REGION_APAC_NE1);
+  $ec2->set_region(AmazonEC2::REGION_APAC_NE1);
   $ec2->terminate_instances($argv[1]);
